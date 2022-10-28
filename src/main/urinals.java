672592430c0644d983774452da31a2e22325aa49
Arshak Shan <arshakshan@gmail.com>
2022-10-27 18:43:55 -0700

    File exists test

--- src/main/urinals.java
+++ src/main/urinals.java
@@ -1,6 +1,11 @@
 package main;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class urinals {
@@ -11,10 +16,14 @@ public class urinals {
     public static void main(String[] args) {
 
 
+
     }
 
 
     public static int freeUrinals(String str) {
+
+        if(!goodStringLength(str) || !goodString(str))
+            return -1;
 
         int stalls = 0;
 
@@ -50,11 +59,22 @@ public class urinals {
         return stalls;
     }
 
+    public static int openFile(){
+
+        Scanner sc;
+        try {
+            BufferedReader bf = new BufferedReader(new FileReader("src/main/urinal.dat"));
+        } catch (FileNotFoundException e) {
+            return -1;
+        }
+        return 1;
+    }
 
     public static String getString(){
 
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
+        String str;
+        str = sc.next();
         return str;
     }
 
@@ -70,6 +90,8 @@ public class urinals {
 
     public static boolean goodString(String str){
 
+        if(str.equals(-1))
+            return false;
         for(int i=0;i<str.length();i++){
             if(str.charAt(i)=='0' || str.charAt(i)=='1'){
                 continue;
--- test/main/urinalsTest.java
+++ test/main/urinalsTest.java
@@ -5,9 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-class urinalsTest {
-
-    urinals ut = new urinals();
+class UrinalsTest {
 
     @Test
     void testOneDummy(){
@@ -18,31 +16,43 @@ class urinalsTest {
     @Test
     void testGetStringLength() {
         System.out.println("====== Arshak Shan == TEST TWO EXECUTED ======");
-//        assertTrue(urinals.goodStringLength("100100000000000000000000000000"), "String length is not right");
-        assertTrue(ut.goodStringLength("1001000"), "String length is not right");
+        assertTrue(urinals.goodStringLength("1001000"), "String length is not right");
     }
 
     @Test
     void testGoodString(){
         System.out.println("====== Arshak Shan == TEST THREE EXECUTED ======");
-        assertTrue(ut.goodString("100001"), "Invalid String");
+        assertTrue(urinals.goodString("100001"), "Invalid String");
     }
 
     @Test
     void testSingleCharString(){
         System.out.println("====== Arshak Shan == TEST FOUR EXECUTED ======");
-        assertTrue(ut.goodString("1"), "Invalid String");
-        assertTrue(ut.goodString("0"), "Invalid String");
+        assertTrue(urinals.goodString("1"), "Invalid String");
+        assertTrue(urinals.goodString("0"), "Invalid String");
+    }
+
+
+    @Test
+    void testEmptyInput(){
+        System.out.println("====== Arshak Shan == TEST FIVE EXECUTED ======");
+        assertTrue(urinals.goodString(""), "Invalid String");
     }
 
     @Test
     void testFreeUrinals(){
-        System.out.println("====== Arshak Shan == TEST FIVE EXECUTED ======");
-        assertEquals(1,ut.freeUrinals("10001"));
-        assertEquals(0,ut.freeUrinals("1001"));
-        assertEquals(3,ut.freeUrinals("00000"));
-        assertEquals(2,ut.freeUrinals("0000"));
-        assertEquals(1,ut.freeUrinals("01000"));
+        System.out.println("====== Arshak Shan == TEST SIX EXECUTED ======");
+        assertEquals(1, urinals.freeUrinals("10001"));
+        assertEquals(0, urinals.freeUrinals("1001"));
+        assertEquals(3, urinals.freeUrinals("00000"));
+        assertEquals(2, urinals.freeUrinals("0000"));
+        assertEquals(1, urinals.freeUrinals("01000"));
+    }
+
+    @Test
+    void testFileExists(){
+        System.out.println("====== Arshak Shan == TEST SEVEN EXECUTED (FILE) ======");
+        assertEquals(1,urinals.openFile(),"File Not Found");
     }
 
 }
