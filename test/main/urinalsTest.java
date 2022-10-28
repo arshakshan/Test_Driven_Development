fe60b2aeac0edb5ca0e28bb4a9017f713e2528fb
Arshak Shan <arshakshan@gmail.com>
2022-10-27 17:38:59 -0700

    new tests added - III

--- test/main/urinalsTest.java
+++ test/main/urinalsTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class urinalsTest {
 
+    urinals ut = new urinals();
+
     @Test
     void testOneDummy(){
         System.out.println("====== Arshak Shan == TEST ONE EXECUTED ======");
@@ -17,28 +19,30 @@ class urinalsTest {
     void testGetStringLength() {
         System.out.println("====== Arshak Shan == TEST TWO EXECUTED ======");
 //        assertTrue(urinals.goodStringLength("100100000000000000000000000000"), "String length is not right");
-        assertTrue(urinals.goodStringLength("1001000"), "String length is not right");
+        assertTrue(ut.goodStringLength("1001000"), "String length is not right");
     }
 
     @Test
     void testGoodString(){
         System.out.println("====== Arshak Shan == TEST THREE EXECUTED ======");
-        assertTrue(urinals.goodString("100001"), "Invalid String");
+        assertTrue(ut.goodString("100001"), "Invalid String");
     }
 
     @Test
     void testSingleCharString(){
         System.out.println("====== Arshak Shan == TEST FOUR EXECUTED ======");
-        assertTrue(urinals.goodString("1"), "Invalid String");
-        assertTrue(urinals.goodString("0"), "Invalid String");
+        assertTrue(ut.goodString("1"), "Invalid String");
+        assertTrue(ut.goodString("0"), "Invalid String");
     }
 
     @Test
     void testFreeUrinals(){
         System.out.println("====== Arshak Shan == TEST FIVE EXECUTED ======");
-        assertEquals(1,urinals.freeUrinals("10001"));
-        assertEquals(0,urinals.freeUrinals("10"));
-
+        assertEquals(1,ut.freeUrinals("10001"));
+        assertEquals(0,ut.freeUrinals("1001"));
+        assertEquals(3,ut.freeUrinals("00000"));
+        assertEquals(2,ut.freeUrinals("0000"));
+        assertEquals(1,ut.freeUrinals("01000"));
     }
 
 }
